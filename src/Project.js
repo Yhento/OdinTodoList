2976c13c4d9654507990269b6bf3c3cd3b245e39
Tyler Payne <154254574+Yhento@users.noreply.github.com>
2024-10-24 11:26:21 -0500

    add Project class.

---
+++ src/Project.js
@@ -0,0 +1,29 @@
+import { Todo } from "./Todo.js";
+
+const Project = (name) => {
+    const project_name = name;
+    let list_of_todos = [];
+
+    const getListOfTodos = () => {
+        return list_of_todos;
+    }
+
+    const addTodo = (title, description, dueDate, priority) => {
+        let new_todo = Todo(title, description, dueDate, priority);
+        list_of_todos.push(new_todo);
+    }
+
+    const editTodo = (todo, title, description, dueDate, priority) => {
+        todo.title = title;
+        todo.description = description;
+        todo.dueDate = dueDate;
+        todo.priority = priority;
+    }
+
+    const removeTodo = (todo) => {
+        list_of_todos.splice(todo, 1);
+    }
+    return {getListOfTodos, addTodo, editTodo, removeTodo};
+}
+
+export{Project};
--- src/Todo.js
+++ src/Todo.js
@@ -2,6 +2,4 @@ const Todo = (title, description, dueDate, priority) => {
     return {title, description, dueDate, priority};
 };
 
-let test_todo = Todo("Todo Title", "This is a test todo.", "10/27/1991", "High");
-
-console.table(test_todo);
+export {Todo};
